5c9d6399ef30bd0a448ac7b21a590b05e6b7eae8
Authenticator <1057237562@outlook.com>
2023-06-27 17:25:00 +0800

    add Framework

---
+++ src/main/java/com/brainsmash/cre/Client.java
@@ -0,0 +1,27 @@
+package com.brainsmash.cre;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class Client implements ClientModInitializer {
+
+    public static final String CATEGORY = "key.category.cre";
+    public static KeyBinding openKey = KeyBindingHelper.registerKeyBinding(
+            new KeyBinding("key.cre.crawl", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M, CATEGORY));
+
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if(openKey.isPressed()){
+                ClientPlayerEntity clientPlayerEntity = MinecraftClient.getInstance().player;
+                client.setScreen(new CraftingRecipeEditScreen(clientPlayerEntity));
+            }
+        });
+    }
+}
---
+++ src/main/java/com/brainsmash/cre/CraftingRecipeEditScreen.java
@@ -0,0 +1,10 @@
+package com.brainsmash.cre;
+
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class CraftingRecipeEditScreen extends CreativeInventoryScreen {
+    public CraftingRecipeEditScreen(PlayerEntity player) {
+        super(player);
+    }
+}
